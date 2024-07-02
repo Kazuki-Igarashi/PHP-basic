302e472565d1cb67b008da72ec12be60c8a12989
Igarashi <vki.0504@gmail.com>
2024-07-02 16:14:48 +0900

    文字列リテラルを作成する

---
+++ string/string01/string01.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>文字列</title>
+</head>
+<body>
+
+<p>
+  <?php echo '<span style="color:#fff000">こんにちは<br> </span>'?>
+  <?php echo "お元気ですか？<br>"; ?>
+  <?php echo "今日は出勤日です<br>"; ?>
+  <?php echo 'Im a student'; ?>
+</p>
+
+
+</body>
