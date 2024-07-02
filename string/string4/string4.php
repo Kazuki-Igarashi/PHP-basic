7f427ce5607cb1ee5ec62b320e983d71b3f47669
Igarashi <vki.0504@gmail.com>
2024-07-02 17:03:10 +0900

    エスケープシーケンスの種類と使い方

---
+++ string/string4/string4.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>エスケープシーケンスの種類と使い方</title>
+</head>
+<body>
+
+  <p>
+    <?php 
+      echo "私の名前は\"本田\"です<br>";
+      echo '私の名前は\'近藤\'です';
+    ?>  
+  </p>
+
+  <pre>
+    <?php echo "こんにちは！\nお元気ですか？";?>
+  </pre>
+
+</body>
