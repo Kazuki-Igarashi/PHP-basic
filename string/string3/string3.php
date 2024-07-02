74daba19856ac8fb017a74d8babcce2b374fc535
Igarashi <vki.0504@gmail.com>
2024-07-02 16:45:25 +0900

    文字列の出力に使用するechoとprintの違い

---
+++ string/string3/string3.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>文字列の出力に使用するechoとprintの違い</title>
+</head>
+<body>
+
+  <?php
+    $pref = "東京都";
+    echo $pref, "港区", "<br>";
+    print "こんにちは"
+  ?>
+
+</body>
