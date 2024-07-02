d910bd2a4e5892a00fd7a0cc548b215d5f8bc882
Igarashi <vki.0504@gmail.com>
2024-07-02 15:21:40 +0900

    PHPに内にコメントを残す

---
+++ sample/coment.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>PHPテスト</title>
+</head>
+<body>
+
+<p>
+  <?php
+      // 作成日 2024/7/2
+      // 作成者 五十嵐一稀
+      // version 3.5
+
+      echo "こんにちは"; # 画面に出力
+    ?>
+</p>
+
+</body>
